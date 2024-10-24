10def33fca0a164f42b9f7e64bbb971817743292
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-10-24 15:26:12 +0900

    Analyse all dirs

--- src/main/java/com/autonomouslogic/evecsmanalysis/Main.java
+++ src/main/java/com/autonomouslogic/evecsmanalysis/Main.java
@@ -3,6 +3,7 @@ package com.autonomouslogic.evecsmanalysis;
 import com.autonomouslogic.evecsmanalysis.models.BallotFile;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.stream.Stream;
 import lombok.SneakyThrows;
@@ -12,10 +13,29 @@ public class Main {
 
 	@SneakyThrows
 	public static void main(String[] args) {
-		var ballotFile = parseBallotFile(new File("csm18/votes.blt"));
-		objectMapper.writerWithDefaultPrettyPrinter().writeValue(new File("csm18/votes.json"), ballotFile);
+		for (var csmDir : getAllCsmDirs().toList()) {
+			processCsmDir(csmDir);
+		}
+	}
+
+	@SneakyThrows
+	private static void processCsmDir(File csmDir) {
+		var csmNumber = Integer.parseInt(csmDir.getName().substring(3));
+		if (csmNumber < 1) {
+			throw new IllegalArgumentException("Invalid CSM number: " + csmNumber + " on dir: " + csmDir);
+		}
+		var votesBlt = new File(csmDir, "votes.blt");
+		if (!votesBlt.exists()) {
+			throw new FileNotFoundException(votesBlt.toString());
+		}
+		var votesJson = new File(csmDir, "votes.json");
+		var readme = new File(csmDir, "Readme.md");
+
+		var ballotFile = parseBallotFile(votesBlt);
+		objectMapper.writerWithDefaultPrettyPrinter().writeValue(votesJson, ballotFile);
+
 		var data = new AnalysisRunner(ballotFile).run();
-		new AnalysisRenderer(18, data).render(new File("csm18/Readme.md"));
+		new AnalysisRenderer(csmNumber, data).render(readme);
 	}
 
 	private static BallotFile parseBallotFile(File file) {
