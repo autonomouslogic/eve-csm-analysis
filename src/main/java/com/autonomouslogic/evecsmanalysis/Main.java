6b2493d4f3a07c62010fe11f5f936cfdcabf9f7c
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-10-24 15:15:50 +0900

    Test all sets

--- src/main/java/com/autonomouslogic/evecsmanalysis/Main.java
+++ src/main/java/com/autonomouslogic/evecsmanalysis/Main.java
@@ -3,7 +3,16 @@ package com.autonomouslogic.evecsmanalysis;
 import com.autonomouslogic.evecsmanalysis.models.BallotFile;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 import lombok.SneakyThrows;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.FalseFileFilter;
+import org.apache.commons.io.filefilter.FileFileFilter;
+import org.apache.commons.io.filefilter.IOFileFilter;
+import org.apache.commons.io.filefilter.PrefixFileFilter;
+import org.apache.commons.io.filefilter.TrueFileFilter;
 
 public class Main {
 	private static final ObjectMapper objectMapper = new ObjectMapper();
@@ -18,5 +27,12 @@ public class Main {
 
 	private static BallotFile parseBallotFile(File file) {
 		return new BallotParser(file).parse();
+	}
+
+	public static Stream<File> getAllCsmDirs() {
+		return Arrays.stream(new File(".").listFiles())
+			.filter(f -> f.isDirectory())
+			.filter(d -> d.getName().startsWith("csm"))
+			.sorted();
 	}
 }
