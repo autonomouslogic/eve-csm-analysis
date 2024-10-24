361cbe6ed79942d8a8a10208c75550b87efff474
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-10-24 10:45:14 +0900

    Basic Thymeleaf setup

---
+++ src/main/java/com/autonomouslogic/evecsmanalysis/AnalysisRenderer.java
@@ -0,0 +1,54 @@
+package com.autonomouslogic.evecsmanalysis;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.Locale;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.context.IContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+@RequiredArgsConstructor
+public class AnalysisRenderer {
+	private final int csmNumber;
+	private final File ballotFilePath;
+
+	@SneakyThrows
+	public void render(File outputFile) {
+		var engine = createEngine();
+		try (var writer = new FileWriter(outputFile, StandardCharsets.UTF_8)) {
+			engine.process("analysis.md", createContext(), writer);
+		}
+	}
+
+	private IContext createContext() {
+		var context = new Context(Locale.ENGLISH);
+		context.setVariable("csmNumber", csmNumber);
+		return context;
+	}
+
+	private TemplateEngine createEngine() {
+		var engine = new TemplateEngine();
+		engine.addTemplateResolver(createResolver(TemplateMode.TEXT, true, ""));
+		return engine;
+	}
+
+	private static ClassLoaderTemplateResolver createResolver(
+			TemplateMode mode, boolean checkExistence, String suffix) {
+		var resolver = new ClassLoaderTemplateResolver();
+		resolver.setCheckExistence(checkExistence);
+		resolver.setPrefix("/templates/");
+		if (suffix != null) {
+			resolver.setSuffix(suffix);
+		}
+		resolver.setTemplateMode(mode);
+		resolver.setCacheable(true);
+		resolver.setCacheTTLMs(Duration.ofDays(1000).toMillis());
+		return resolver;
+	}
+}
--- src/main/java/com/autonomouslogic/evecsmanalysis/Main.java
+++ src/main/java/com/autonomouslogic/evecsmanalysis/Main.java
@@ -1,7 +1,9 @@
 package com.autonomouslogic.evecsmanalysis;
 
+import java.io.File;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		new AnalysisRenderer(18, new File("csm18/votes.blt")).render(new File("csm18/index.md"));
 	}
 }
